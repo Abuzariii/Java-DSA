5047eb121636e8da559d0e9a6b04ffe57907bfbe
Muhammad Abuar <muhammadabuzar010@gmail.com>
2023-07-10 11:25:36 +0500

    Fibonacci sequence with recursion

---
+++ Recursion/src/Fibo.java
@@ -0,0 +1,12 @@
+public class Fibo {
+    public static void main(String[] args) {
+        System.out.println(fibonacci(6));
+    }
+
+    static int fibonacci(int n) {
+        if (n < 2) {
+            return n;
+        }
+        return fibonacci(n - 1) + fibonacci(n - 2);
+    }
+}
