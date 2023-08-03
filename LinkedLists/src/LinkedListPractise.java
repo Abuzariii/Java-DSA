32c0ae935dce3b85edd01a7878453667c5fb9207
Muhammad Abuar <muhammadabuzar010@gmail.com>
2023-08-03 15:24:36 +0500

    Some recursion

--- LinkedLists/src/LinkedListPractise.java
+++ LinkedLists/src/LinkedListPractise.java
@@ -28,6 +28,11 @@ class Node {
         this.value = value;
         this.next = null;
     }
+
+    public Node(int value, Node next) {
+        this.value = value;
+        this.next = null;
+    }
 }
 
 class LinkedList {
@@ -114,6 +119,75 @@ class LinkedList {
         return current;
     }
 
+    public void insertAt(int index, int value) {
+        // Create a new node with given value
+        // Get the node at given index and index - 1
+        // Point the next of index-1 to new node and next of new node to index
+        // Edge case is if the index is smaller than size
+        if (index < size) {
+            Node node = new Node(value);
+            Node indexNode = get(index);
+            Node prevNode = get(index - 1);
+            prevNode.next = node;
+            node.next = indexNode;
+            size++;
+        } else {
+            System.out.println("Index exceeds the size");
+        }
+    }
+
+    // Insert recursion, my own implementation
+    public void insertRec(int index, int value, Node current, int count, Node prev) {
+        // End of the list case
+        if (current == null) {
+            if (index == count) {
+                Node node = new Node(value);
+                prev.next = node;
+                node.next = null;
+                size++;
+            }
+            return;
+        }
+        // Start of the list case
+        if (index == 0) {
+            Node node = new Node(value);
+            node.next = current;
+            size++;
+            if (prev == null) {
+                head = node;
+            } else {
+                prev.next = node;
+            }
+            return;
+        }
+        // Between the list case
+        if (index == count) {
+            Node node = new Node(value);
+            prev.next = node;
+            node.next = current;
+            size++;
+            return;
+        }
+        count++;
+        insertRec(index, value, current.next, count, current);
+    }
+
+    // Insert using recursion, Kunal Kushwaha
+    public void insertRec(int val, int index) {
+        head = insertRec(val, index, head);
+    }
+
+    private Node insertRec(int val, int index, Node node) {
+        if (index == 0) {
+            Node temp = new Node(val, node);
+            size++;
+            return temp;
+        }
+
+        node.next = insertRec(val, index - 1, node.next);
+        return node;
+    }
+
     public void addAfter(int value, int index) {
         // Create new node with given value
         // Find the element at given index
@@ -160,6 +234,15 @@ class LinkedList {
         System.out.println("Size : " + size);
     }
 
+    public void displayRec(Node node) {
+        if (node == null) {
+            System.out.println("NULL");
+            return;
+        }
+        System.out.print(node.value + " --> ");
+        displayRec(node.next);
+    }
+
     // in place reversal of linked list
     // google, microsoft, apple, amazon:
     // https://leetcode.com/problems/reverse-linked-list/
@@ -181,5 +264,21 @@ class LinkedList {
             }
         }
         head = prev;
+    }
+
+    // Point the node in the arguement to the head of the list
+    public void reverseRec(Node node) {
+        if (node == tail) {
+            head = tail;
+            return;
+        }
+        // Go to the end of the list
+        // At the end you will be at the tail, point next poitner of tail to current
+        // node
+        // Now move tail pointer to the node and the next of the tail to null
+        reverseRec(node.next);
+        tail.next = node;
+        tail = node;
+        tail.next = null;
     }
 }
